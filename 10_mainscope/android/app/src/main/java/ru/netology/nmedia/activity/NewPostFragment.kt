10ee69abda03c99a6e9e18eea5fbe98eef4c87d8
RusOneGit <shaparev.daniel@gmail.com>
2025-01-30 17:37:44 +0200

    flow

--- 10_mainscope/android/app/src/main/java/ru/netology/nmedia/activity/NewPostFragment.kt
+++ 10_mainscope/android/app/src/main/java/ru/netology/nmedia/activity/NewPostFragment.kt
@@ -2,12 +2,25 @@ package ru.netology.nmedia.activity
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.net.toFile
+import androidx.core.view.MenuProvider
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import com.github.dhaval2404.imagepicker.ImagePicker
+import ru.netology.nmedia.R
 import ru.netology.nmedia.databinding.FragmentNewPostBinding
+import ru.netology.nmedia.model.PhotoModel
 import ru.netology.nmedia.util.AndroidUtils
 import ru.netology.nmedia.util.StringArg
 import ru.netology.nmedia.viewmodel.PostViewModel
@@ -16,6 +29,7 @@ class NewPostFragment : Fragment() {
 
     companion object {
         var Bundle.textArg: String? by StringArg
+        private const val IMAGE_MAX_SIZE = 2048
     }
 
     private val viewModel: PostViewModel by activityViewModels()
@@ -34,14 +48,72 @@ class NewPostFragment : Fragment() {
         arguments?.textArg
             ?.let(binding.edit::setText)
 
-        binding.ok.setOnClickListener {
-            viewModel.changeContent(binding.edit.text.toString())
-            viewModel.save()
-            AndroidUtils.hideKeyboard(requireView())
+        requireActivity().addMenuProvider(
+            object : MenuProvider {
+                override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                    menuInflater.inflate(R.menu.new_post_menu, menu)
+                }
+
+                override fun onMenuItemSelected(menuItem: MenuItem): Boolean =
+                    if (menuItem.itemId == R.id.save) {
+                        viewModel.changeContent(binding.edit.text.toString())
+                        viewModel.save()
+                        AndroidUtils.hideKeyboard(requireView())
+                        true
+                    } else {
+                        false
+
+                    }
+            },
+            viewLifecycleOwner,
+        )
+
+        viewModel.photo.observe(viewLifecycleOwner) {
+            if (it == null) {
+                binding.previewContainer.isGone = true
+                return@observe
+            } else {
+                binding.previewContainer.isVisible = true
+                binding.preview.setImageURI(it.uri)
+            }
         }
+
+        val photoResultContract =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                if (it.resultCode == ImagePicker.RESULT_ERROR) {
+                    Toast.makeText(
+                        requireContext(),
+                        getString(R.string.image_picker_error), Toast.LENGTH_SHORT
+                    ).show()
+                    return@registerForActivityResult
+                }
+                val uri = it.data?.data ?: return@registerForActivityResult
+
+                viewModel.savePhoto(PhotoModel(uri, uri.toFile()))
+            }
+
+        binding.pickPhoto.setOnClickListener {
+            ImagePicker.Builder(this)
+                .crop()
+                .maxResultSize(IMAGE_MAX_SIZE, IMAGE_MAX_SIZE)
+                .galleryOnly()
+                .createIntent(photoResultContract::launch)
+
+        }
+
+        binding.takePhoto.setOnClickListener {
+            ImagePicker.Builder(this)
+                .crop()
+                .maxResultSize(IMAGE_MAX_SIZE, IMAGE_MAX_SIZE)
+                .cameraOnly()
+                .createIntent(photoResultContract::launch)
+
+        }
+
         viewModel.postCreated.observe(viewLifecycleOwner) {
             findNavController().navigateUp()
         }
         return binding.root
     }
 }
+
--- 10_mainscope/android/app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
+++ 10_mainscope/android/app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -9,6 +9,7 @@ import ru.netology.nmedia.db.AppDb
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.model.FeedModel
 import ru.netology.nmedia.model.FeedModelState
+import ru.netology.nmedia.model.PhotoModel
 import ru.netology.nmedia.repository.PostRepository
 import ru.netology.nmedia.repository.PostRepositoryImpl
 import ru.netology.nmedia.util.SingleLiveEvent
@@ -42,6 +43,9 @@ class PostViewModel(application: Application) : AndroidViewModel(application) {
     private val _postCreated = SingleLiveEvent<Unit>()
     val postCreated: LiveData<Unit>
         get() = _postCreated
+
+    private val _photo = MutableLiveData<PhotoModel>(null)
+    val photo:LiveData<PhotoModel?> = _photo
 
     init {
         loadPosts()
@@ -111,5 +115,10 @@ class PostViewModel(application: Application) : AndroidViewModel(application) {
                 _dataState.value = FeedModelState(error = true)
             }
         }
+    }
+
+    fun savePhoto(photoModel: PhotoModel) {
+        _photo.value = photoModel
+
     }
 }
